916d0eb928703a9802afc21c7054190667069072
妖雨纯 <876451336@qq.com>
2022-11-10 23:14:31 +0800

    ✨ feat(Base): 修改基础数据格式

--- src/views/common/data.js
+++ src/views/common/data.js
@@ -1,153 +1,260 @@
 export const source = [
     {
         id: '72f29b4f-e7c5-4107-8447-d3a7f778e16c',
+        max: -1,
         name: '电子邮件',
         icon: 'el-icon-message',
         type: 'MESSAGE',
         style: { backgroundColor: '#e1f1ff', color: '#75BAF9' },
-        rules: [{ id: 'be1aa742-84c2-441f-88c6-0fa93f9bd474', type: 'info', content: '' }],
+        rules: [{ id: 'be1aa742-84c2-441f-88c6-0fa93f9bd474', max: 2, type: 'info', content: '' }],
         connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
     },
     {
         id: '8a1dab33-e4ab-4830-a6ac-10aa2d86e7d2',
+        max: -1,
         name: '触发器',
         icon: 'el-icon-cpu',
         type: 'CPU',
         style: { backgroundColor: '#ffe1e1', color: '#F78C88' },
         rules: [
-            { id: '8650db59-8fff-4246-ba0a-e1b0b198d4c3', type: 'danger', content: 'NO' },
-            { id: '790f973f-e407-4d49-ba95-784a052bfc65', type: 'success', content: 'YES' }
-        ]
+            { id: '8650db59-8fff-4246-ba0a-e1b0b198d4c3', max: 1, type: 'danger', content: 'NO' },
+            { id: '790f973f-e407-4d49-ba95-784a052bfc65', max: 1, type: 'success', content: 'YES' }
+        ],
+        connect: ['MESSAGE', 'CLOCK', 'PRESENT']
     },
     {
         id: 'f7ff1dcb-cde7-4f83-b33d-b14f913160a4',
+        max: -1,
         name: '延时',
         icon: 'el-icon-alarm-clock',
         type: 'CLOCK',
         style: { backgroundColor: '#fbf1bd', color: '#EDB342' },
-        rules: [{ id: 'db2deb1a-5f3d-4458-9c34-4f55d9d53788', type: 'info', content: '' }]
+        rules: [{ id: 'db2deb1a-5f3d-4458-9c34-4f55d9d53788', max: 1, type: 'info', content: '' }],
+        connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
     },
     {
         id: 'f4074fc3-b4ef-4b14-8cf9-32b00b32037e',
+        max: -1,
         name: '目标',
         icon: 'el-icon-present',
         type: 'PRESENT',
-        style: { backgroundColor: '#e6e0ff', color: '#8E72F7' }
+        style: { backgroundColor: '#e6e0ff', color: '#8E72F7' },
+        connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
     }
 ]
 
 export const column = [
     {
         props: {
-            id: '4d872f9c-7f65-43fc-85ae-0c01eb7323f9',
+            id: '72f29b4f-e7c5-4107-8447-d3a7f778e16c',
+            max: -1,
             name: '电子邮件',
             icon: 'el-icon-message',
             type: 'MESSAGE',
             style: { backgroundColor: '#e1f1ff', color: '#75BAF9' },
-            rules: [{ id: 'ef713561-4659-406d-b165-38eca2f1cd95', type: 'info', content: '' }]
+            rules: [{ id: 'be1aa742-84c2-441f-88c6-0fa93f9bd474', max: 2, type: 'info', content: '' }],
+            connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
         },
-        rules: [{ id: 'd65fe315-f62c-4e85-a5f2-629fa0170cd9', type: 'info', content: '' }],
-        id: '024247fb-2403-465a-abc0-7648873442bb',
-        top: '100px',
-        left: '300px'
+        rules: [{ id: '824f65ff-ba00-425f-9942-e808466ed7a7', max: 2, type: 'info', content: '' }],
+        id: 'c86f6f22-28e1-4c38-8db8-3d5fa99271e3',
+        top: '45px',
+        left: '645px'
     },
     {
         props: {
-            id: '77cd0f97-7cc3-4554-855c-78ca230e6187',
+            id: '8a1dab33-e4ab-4830-a6ac-10aa2d86e7d2',
+            max: -1,
             name: '触发器',
             icon: 'el-icon-cpu',
             type: 'CPU',
             style: { backgroundColor: '#ffe1e1', color: '#F78C88' },
             rules: [
-                { id: '2233e175-b36b-4c94-9897-8806793a34b3', type: 'danger', content: 'NO' },
-                { id: '3aa44c05-e324-4f21-9c81-9bb7aca1f6f3', type: 'success', content: 'YES' }
-            ]
+                { id: '8650db59-8fff-4246-ba0a-e1b0b198d4c3', max: 1, type: 'danger', content: 'NO' },
+                { id: '790f973f-e407-4d49-ba95-784a052bfc65', max: 1, type: 'success', content: 'YES' }
+            ],
+            connect: ['MESSAGE', 'CLOCK', 'PRESENT']
         },
         rules: [
-            { id: '68d1ddf6-c7c8-4abb-ae47-060e7f770193', type: 'danger', content: 'NO' },
-            { id: '43f9ffb1-8a69-4d12-9d87-ad16eb5016f4', type: 'success', content: 'YES' }
+            { id: '8344c059-3632-45f9-952f-4fa07671b9e2', max: 1, type: 'danger', content: 'NO' },
+            { id: '4578bc01-25cb-4724-b92b-3455987db3ad', max: 1, type: 'success', content: 'YES' }
         ],
-        id: '4e5d9cb5-65a9-4163-a723-3ed137fd9e5d',
-        top: '300px',
-        left: '500px'
+        id: '998529b4-e050-4702-abf6-726d12529def',
+        top: '240px',
+        left: '870px'
     },
     {
         props: {
-            id: '1c47b773-5cc9-4610-91de-b77979143008',
+            id: 'f7ff1dcb-cde7-4f83-b33d-b14f913160a4',
+            max: -1,
             name: '延时',
             icon: 'el-icon-alarm-clock',
             type: 'CLOCK',
             style: { backgroundColor: '#fbf1bd', color: '#EDB342' },
-            rules: [{ id: '5ee6271c-47e4-492d-a055-85be9b145556', type: 'info', content: '' }]
+            rules: [{ id: 'db2deb1a-5f3d-4458-9c34-4f55d9d53788', max: 1, type: 'info', content: '' }],
+            connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
         },
-        rules: [{ id: '96ebbf69-f544-4bea-ad7b-b513f0abca95', type: 'info', content: '' }],
-        id: 'c9736f2a-af8c-43f0-8d88-1c532b9c9707',
-        top: '560px',
-        left: '200px'
+        rules: [{ id: '3f8b2bca-a320-40e5-a73d-ac51d952db32', max: 1, type: 'info', content: '' }],
+        id: 'da681bf8-b93d-4dcc-9e81-cbe13b34e1ea',
+        top: '490px',
+        left: '600px'
     },
     {
         props: {
-            id: '1ffc5ba9-2c34-48bb-ac46-7f71cc39ee72',
+            id: '72f29b4f-e7c5-4107-8447-d3a7f778e16c',
+            max: -1,
             name: '电子邮件',
             icon: 'el-icon-message',
             type: 'MESSAGE',
             style: { backgroundColor: '#e1f1ff', color: '#75BAF9' },
-            rules: [{ id: 'd0e96ef2-968c-4ff6-b027-62dcaa8e3d6d', type: 'info', content: '' }]
+            rules: [{ id: 'be1aa742-84c2-441f-88c6-0fa93f9bd474', max: 2, type: 'info', content: '' }],
+            connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
         },
-        rules: [{ id: '53603549-521f-445b-b8af-18547157c311', type: 'info', content: '' }],
-        id: '8473cca3-e9ce-42e4-9808-6f33e5e0199c',
-        top: '570px',
-        left: '715px'
+        rules: [{ id: 'be7fa609-74d1-4f75-ad8f-a4d34b64320e', max: 2, type: 'info', content: '' }],
+        id: 'a57be178-99fb-4537-b519-bca5add27bbd',
+        top: '490px',
+        left: '1055px'
     },
     {
         props: {
-            id: '67084df0-a7c7-4bfe-aa61-7c213dfdc314',
+            id: '8a1dab33-e4ab-4830-a6ac-10aa2d86e7d2',
+            max: -1,
+            name: '触发器',
+            icon: 'el-icon-cpu',
+            type: 'CPU',
+            style: { backgroundColor: '#ffe1e1', color: '#F78C88' },
+            rules: [
+                { id: '8650db59-8fff-4246-ba0a-e1b0b198d4c3', max: 1, type: 'danger', content: 'NO' },
+                { id: '790f973f-e407-4d49-ba95-784a052bfc65', max: 1, type: 'success', content: 'YES' }
+            ],
+            connect: ['MESSAGE', 'CLOCK', 'PRESENT']
+        },
+        rules: [
+            { id: 'e97e7236-a38c-41dc-9187-1511d65a05ba', max: 1, type: 'danger', content: 'NO' },
+            { id: 'ff61f6b9-4a80-415a-9d7a-d2020e8a991e', max: 1, type: 'success', content: 'YES' }
+        ],
+        id: 'aa812ab3-2066-4421-a0d7-f61da4f0c014',
+        top: '695px',
+        left: '925px'
+    },
+    {
+        props: {
+            id: '72f29b4f-e7c5-4107-8447-d3a7f778e16c',
+            max: -1,
+            name: '电子邮件',
+            icon: 'el-icon-message',
+            type: 'MESSAGE',
+            style: { backgroundColor: '#e1f1ff', color: '#75BAF9' },
+            rules: [{ id: 'be1aa742-84c2-441f-88c6-0fa93f9bd474', max: 2, type: 'info', content: '' }],
+            connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
+        },
+        rules: [{ id: '91ea0846-bdb8-4b43-ad9a-5ef6bc36bbcc', max: 2, type: 'info', content: '' }],
+        id: 'd47eca73-12eb-4627-b3d6-83437902c4bb',
+        top: '730px',
+        left: '435px'
+    },
+    {
+        props: {
+            id: 'f4074fc3-b4ef-4b14-8cf9-32b00b32037e',
+            max: -1,
             name: '目标',
             icon: 'el-icon-present',
             type: 'PRESENT',
-            style: { backgroundColor: '#e6e0ff', color: '#8E72F7' }
+            style: { backgroundColor: '#e6e0ff', color: '#8E72F7' },
+            connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
         },
         rules: [],
-        id: 'c257d284-b163-41c4-af30-5f5fff819c7e',
-        top: '780px',
-        left: '470px'
+        id: '91907c25-d5a8-4a8d-bf48-9a5c18db1eff',
+        top: '1020px',
+        left: '215px'
+    },
+    {
+        props: {
+            id: '72f29b4f-e7c5-4107-8447-d3a7f778e16c',
+            max: -1,
+            name: '电子邮件',
+            icon: 'el-icon-message',
+            type: 'MESSAGE',
+            style: { backgroundColor: '#e1f1ff', color: '#75BAF9' },
+            rules: [{ id: 'be1aa742-84c2-441f-88c6-0fa93f9bd474', max: 2, type: 'info', content: '' }],
+            connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
+        },
+        rules: [{ id: '303435bf-eeec-4f2c-b808-5fdebb3af1f4', max: 2, type: 'info', content: '' }],
+        id: '9f676d77-d432-4d52-9c00-9baef041b38e',
+        top: '980px',
+        left: '1115px'
+    },
+    {
+        props: {
+            id: 'f4074fc3-b4ef-4b14-8cf9-32b00b32037e',
+            max: -1,
+            name: '目标',
+            icon: 'el-icon-present',
+            type: 'PRESENT',
+            style: { backgroundColor: '#e6e0ff', color: '#8E72F7' },
+            connect: ['MESSAGE', 'CPU', 'CLOCK', 'PRESENT']
+        },
+        rules: [],
+        id: '7be2d223-3298-47b2-96b5-97c2bc409cf8',
+        top: '955px',
+        left: '660px'
     }
 ]
 
 export const line = [
     {
-        id: 'ee05f469-aff7-4c12-bb80-cd34662a98df',
-        parent: '024247fb-2403-465a-abc0-7648873442bb',
-        source: 'd65fe315-f62c-4e85-a5f2-629fa0170cd9',
-        target: '4e5d9cb5-65a9-4163-a723-3ed137fd9e5d',
+        id: 'ddbd3bdb-12ee-44f3-bd92-ce791c7d2502',
+        parent: 'c86f6f22-28e1-4c38-8db8-3d5fa99271e3',
+        source: '824f65ff-ba00-425f-9942-e808466ed7a7',
+        target: '998529b4-e050-4702-abf6-726d12529def',
         label: '猪头'
     },
     {
-        id: '2e429fe4-bf9c-4806-8e7b-0ad43181aaad',
-        parent: '4e5d9cb5-65a9-4163-a723-3ed137fd9e5d',
-        source: '68d1ddf6-c7c8-4abb-ae47-060e7f770193',
-        target: 'c9736f2a-af8c-43f0-8d88-1c532b9c9707',
+        id: '704ca660-c7f9-4107-9e34-723099be4b29',
+        parent: '998529b4-e050-4702-abf6-726d12529def',
+        source: '8344c059-3632-45f9-952f-4fa07671b9e2',
+        target: 'da681bf8-b93d-4dcc-9e81-cbe13b34e1ea',
         label: '猪头'
     },
     {
-        id: '7d31d831-0219-41df-aca9-c4f3e5c2bcb8',
-        parent: '4e5d9cb5-65a9-4163-a723-3ed137fd9e5d',
-        source: '43f9ffb1-8a69-4d12-9d87-ad16eb5016f4',
-        target: '8473cca3-e9ce-42e4-9808-6f33e5e0199c',
+        id: 'c631ea19-fe72-4950-ac98-1a5e6d23c9cd',
+        parent: '998529b4-e050-4702-abf6-726d12529def',
+        source: '4578bc01-25cb-4724-b92b-3455987db3ad',
+        target: 'a57be178-99fb-4537-b519-bca5add27bbd',
         label: '猪头'
     },
     {
-        id: '59a005ba-46f1-4f85-9a01-9aa8a8ec68d2',
-        parent: '8473cca3-e9ce-42e4-9808-6f33e5e0199c',
-        source: '53603549-521f-445b-b8af-18547157c311',
-        target: 'c257d284-b163-41c4-af30-5f5fff819c7e',
+        id: '74debb05-e1eb-46f5-9a35-6ba4ed9f8660',
+        parent: 'a57be178-99fb-4537-b519-bca5add27bbd',
+        source: 'be7fa609-74d1-4f75-ad8f-a4d34b64320e',
+        target: 'aa812ab3-2066-4421-a0d7-f61da4f0c014',
         label: '猪头'
     },
     {
-        id: 'bfbb3dbe-ec3b-4573-bb49-81a0181dc00c',
-        parent: 'c9736f2a-af8c-43f0-8d88-1c532b9c9707',
-        source: '96ebbf69-f544-4bea-ad7b-b513f0abca95',
-        target: 'c257d284-b163-41c4-af30-5f5fff819c7e',
+        id: 'c5f0b150-4c24-472a-acc6-00415126eeb2',
+        parent: 'da681bf8-b93d-4dcc-9e81-cbe13b34e1ea',
+        source: '3f8b2bca-a320-40e5-a73d-ac51d952db32',
+        target: 'd47eca73-12eb-4627-b3d6-83437902c4bb',
+        label: '猪头'
+    },
+    {
+        id: 'cb319d8f-c9ba-4dda-a65b-7d63c1745b7a',
+        parent: 'd47eca73-12eb-4627-b3d6-83437902c4bb',
+        source: '91ea0846-bdb8-4b43-ad9a-5ef6bc36bbcc',
+        target: '91907c25-d5a8-4a8d-bf48-9a5c18db1eff',
+        label: '猪头'
+    },
+    {
+        id: '3212ae44-9dfb-43a3-8153-306ceec5b649',
+        parent: 'aa812ab3-2066-4421-a0d7-f61da4f0c014',
+        source: 'ff61f6b9-4a80-415a-9d7a-d2020e8a991e',
+        target: '9f676d77-d432-4d52-9c00-9baef041b38e',
+        label: '猪头'
+    },
+    {
+        id: '86eca0be-a6fc-4268-992f-c85004a04bba',
+        parent: 'aa812ab3-2066-4421-a0d7-f61da4f0c014',
+        source: 'e97e7236-a38c-41dc-9187-1511d65a05ba',
+        target: '7be2d223-3298-47b2-96b5-97c2bc409cf8',
         label: '猪头'
     }
 ]
