d3d0fb51094bb1bbe7f8c3576f323704a7482028
妖雨纯 <876451336@qq.com>
2022-12-07 22:34:59 +0800

    ✨ feat(Base): 基础事件新增过滤配置

--- src/core/utils/utils-click-outside.js
+++ src/core/utils/utils-click-outside.js
@@ -1,5 +1,5 @@
 function validate(binding) {
-    if (typeof binding.value !== 'function') {
+    if (typeof binding.value.handler !== 'function') {
         console.warn('[Vue-click-outside:] provided expression', binding.expression, 'is not a function.')
         return false
     }
@@ -30,6 +30,12 @@ function isServer(vNode) {
     return typeof vNode.componentInstance !== 'undefined' && vNode.componentInstance.$isServer
 }
 
+function isNodeWhite(e, className) {
+    return (e.path ?? []).some(el => {
+        return Array.from(el.classList).some(name => className.includes(name))
+    })
+}
+
 export const ClickOutside = {
     bind: function (el, binding, vNode) {
         if (!validate(binding)) return
@@ -42,7 +48,13 @@ export const ClickOutside = {
             var elements = e.path || (e.composedPath && e.composedPath())
             elements && elements.length > 0 && elements.unshift(e.target)
 
-            if (el.contains(e.target) || isPopup(vNode.context.popupItem, elements)) return
+            if (el.contains(e.target) || isPopup(vNode.context.popupItem, elements)) {
+                return false
+            }
+
+            if (binding.value.option?.white && isNodeWhite(e, binding.value.option)) {
+                return false
+            }
 
             el?.__vueClickOutside__?.callback?.(e)
         }
@@ -50,13 +62,13 @@ export const ClickOutside = {
         // add Event Listeners
         el.__vueClickOutside__ = {
             handler: handler,
-            callback: binding.value
+            callback: binding.value.handler
         }
         const clickHandler = 'ontouchstart' in document.documentElement ? 'touchstart' : 'click'
-        !isServer(vNode) && document.addEventListener(clickHandler, handler, true)
+        !isServer(vNode) && document.addEventListener(clickHandler, handler, binding.value.option?.catch ?? true)
     },
     update: function (el, binding) {
-        if (validate(binding)) el.__vueClickOutside__.callback = binding.value
+        if (validate(binding)) el.__vueClickOutside__.callback = binding.value.handler
     },
     unbind: function (el, binding, vNode) {
         // Remove Event Listeners
